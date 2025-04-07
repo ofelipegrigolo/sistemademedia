ec9b5d50b6546dba2253ac61fd8abacf12c6538a
Felipe R Grigolo <grigolodev@gmail.com>
2025-04-07 17:18:54 -0300

    v6

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ function Media() {
   const kminicial = parseFloat(document.getElementById("km_inicial").value);  
   const kmfinal = parseFloat(document.getElementById("km_final").value);  
   const qtdabastecida = parseFloat(document.getElementById("qtd_abastecida").value);
-  const mediavalor = document.getElementById("mediavalor");  
-
+  const mediavalor = document.getElementById("mediavalor"); 
 
   const media = (kmfinal - kminicial) / qtdabastecida;  
   
@@ -14,3 +13,10 @@ function Media() {
 const btn = document.getElementById("btn");  
 btn.addEventListener("click", Media);  
 
+function Registro(){
+  const cardAbastecimento = document.getElementById("card-abastecimento");
+  cardAbastecimento.style.visibility = "visible";
+}
+
+const btnRegistro = document.getElementById("btncadastro");
+btnRegistro.addEventListener("click",Registro);
